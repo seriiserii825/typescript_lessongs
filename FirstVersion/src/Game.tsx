e293b5a4d357ebbab68e2335f0ebc53a4ff29169
serii burduja <seriiburduja@gmail.com>
2025-03-07 12:21:37 +0200

    feat: game ts

--- FirstVersion/src/Game.tsx
+++ FirstVersion/src/Game.tsx
@@ -1,12 +1,16 @@
+import { FC } from "react";
+import { TGame } from "./data";
 
-function Game({ name, description, version }) {
+type TGameProps = Pick<TGame, 'name' |'description' | 'version'>;
+
+const Game: FC<TGameProps> = ({ name, description, version }) => {
   return (
     <div>
-     <p>Имя: {name}</p>
-     <p>Описание: {description}</p>
-     <p>Версия: {version}</p>
+      <p>Имя: {name}</p>
+      <p>Описание: {description}</p>
+      <p>Версия: {version}</p>
     </div>
-  )
+  );
 };
 
 export default Game;
